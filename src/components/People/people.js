c0d851974d2599261b01773364e84b96518b8fe4
nd55 <45429767+nd55@users.noreply.github.com>
2022-02-15 11:59:17 +0100

    people section (without images)

---
+++ src/components/People/people.js
@@ -0,0 +1,26 @@
+export const people = [
+  {
+    name: "Vanessa Walter",
+    jobTitle: "Job Title",
+  },
+  {
+    name: "Christian Walter",
+    jobTitle: "Job Title",
+    twitter: "nablahero",
+  },
+  {
+    name: "Eray Basar",
+    jobTitle: "Job Title",
+    twitter: "toshiyori",
+  },
+  {
+    name: "Sebastian Deutsch",
+    jobTitle: "Job Title",
+    twitter: "sippndipp",
+  },
+  {
+    name: "Tommi Gutscher",
+    jobTitle: "Job Title",
+    twitter: "t0mmig",
+  },
+]
