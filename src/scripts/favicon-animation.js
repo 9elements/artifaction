7b921702552054844acee8126d34bb56b0c8a4cc
nd55 <45429767+nd55@users.noreply.github.com>
2022-02-17 01:11:11 +0100

    a11y/usability improvements

---
+++ src/scripts/favicon-animation.js
@@ -0,0 +1,10 @@
+/**
+ * Safari can't handle SVG favicons :(
+ * So only apply svg-favicon animation in non-safari browsers
+ */
+if (window.safari === undefined) {
+  favicon.animate(
+    [...Array(8).keys()].map((i) => `./favicons/favicon-frame-0${i}.svg`),
+    1100
+  )
+}
