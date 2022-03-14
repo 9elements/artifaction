26ef28156438e0f54e294253b49730153f7cc01b
nd55 <45429767+nd55@users.noreply.github.com>
2022-03-14 14:19:10 +0100

    update components + minify scripts

--- public/js/favicon-animation.js
+++ public/js/favicon-animation.js
@@ -2,9 +2,4 @@
  * Safari can't handle SVG favicons :(
  * So only apply svg-favicon animation in non-safari browsers
  */
-if (window.safari === undefined) {
-  favicon.animate(
-    [...Array(8).keys()].map((i) => `./favicons/favicon-frame-${i + 1}.svg`),
-    1100
-  )
-}
+ window.safari===void 0&&favicon.animate([...Array(8).keys()].map(i=>`./favicons/favicon-frame-${i+1}.svg`),1100);
