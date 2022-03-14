557fe6312206a79fd5f77bc58333666d2e1e4f1d
nd55 <45429767+nd55@users.noreply.github.com>
2022-03-14 15:48:54 +0100

    add buildOptions.site

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,4 +13,7 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
     external: ["svgo"],
   },
   renderers: ["@astrojs/renderer-react"],
+  buildOptions: {
+    site: "https://artifaction.netlify.app",
+  },
 })
