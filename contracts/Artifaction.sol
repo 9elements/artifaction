370fcdd7f0ef2fb0e5951c6473141bf91e775822
Christian Walter <christian.walter@9elements.com>
2022-05-30 20:07:54 +0200

    Add Contract and Integration
    This commits adds the initial contracts and integration with react
    
    Signed-off-by: Christian Walter <christian.walter@9elements.com>

---
+++ contracts/Artifaction.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Artifaction is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    uint256 public price = 0.05 ether;
+    uint256 public maxCollection = 10000;
+
+    constructor() ERC721("Artifaction", "ART") {}
+
+    function mintArt(string memory tokenURI) public payable {
+        require(msg.value >= price, "Not enough ETH sent: check price.");
+        _tokenIds.increment();
+
+        uint256 newTokenId = _tokenIds.current();
+        _safeMint(_msgSender(), newTokenId);
+
+        _setTokenURI(newTokenId, tokenURI);
+    }
+
+    function setNewTokenURI(uint256 tokenId, string memory tokenURI) external onlyOwner {
+        require(tokenId != 0);
+
+        _setTokenURI(tokenId, tokenURI);
+    }
+}
